f9c9636f8d9ac8b82e53ee245d6f40050cb9a1c8
Abdallah <abdallahchaar90@gmail.comh>
2021-01-06 18:59:38 +0100

    träning om =

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 //Conditional Operators
 /*
 var myPrice = 100;
@@ -8,10 +7,69 @@ if (myPrice > 50) {
 }
 */
 
+
+/*
 var myPrice = 40;
+
+if (myPrice >= 50) {
+
+    alert("Sorry The Price Is " + myPrice);
+
+} else {
+
+    alert("Good The Price Is" + myPrice);
+}
+*/
+
+/*
+( = ) Assignment Operator
+( == )
+( === )
+*/
+/*
+var myPrice = 50;
+
 if (myPrice > 50) {
 
-    alert("Sorry The Price Is " + myPrice );
-}else {
-    alert("Good The Price Is" + myPrice); 
+    alert("Good");
+
+} else {
+
+    alert("Bad");
+}
+*/
+/*
+var myPrice = 50;
+
+if (myPrice = 50) {
+
+    alert("Good");
+
+} else {
+
+    alert("Bad");
+}
+*/
+/*
+var myPrice = 50;
+
+if (myPrice = 60) {
+
+    alert("Good");
+
+} else {
+
+    alert("Bad");
+}
+*/
+
+var myPrice = 50;
+
+if (myPrice = 600) {
+
+    alert("Good");
+
+} else {
+
+    alert("Bad");
 }
